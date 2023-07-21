d6f2b8b891ae12e0b52a6c5eac9417e0fcf4cbca
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-21 11:37:23 +0700

    modify students routes

--- routes/students.js
+++ routes/students.js
@@ -5,6 +5,5 @@ const { ROLES } = require("../utils/enum");
 
 router.get("/results", authorize([ROLES.Siswa]), students.nilaUlangan);
 router.get("/reports", authorize([ROLES.Siswa]), students.nilaiRaport);
-router.get("/teachers", authorize([ROLES.Siswa]), students.teachers);
-router.get("/classes", authorize([ROLES.Siswa]), students.classes);
+
 module.exports = router;
