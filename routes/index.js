2290828ebd562d82d23da012b2a3736f4fdb9e44
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-21 11:36:55 +0700

    modify index routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const students = require("./students");
 const auth = require("./auth");
+const classes = require("./classes");
+const teachers = require("./teachers");
 
 router.get("/", (req, res) => {
   return res.status(200).json({
@@ -12,5 +14,7 @@ router.get("/", (req, res) => {
 
 router.use("/students", students);
 router.use("/auth", auth);
+router.use("/classes", classes);
+router.use("/teachers", teachers);
 
 module.exports = router;
