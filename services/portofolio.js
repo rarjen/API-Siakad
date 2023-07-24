79a73f6c55bfc827c75d8f6e25dbddc8046f6a1c
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-24 10:00:45 +0700

    make portofolio services

---
+++ services/portofolio.js
@@ -0,0 +1,44 @@
+const { NotFoundError } = require("../errors");
+const { Portofolio, Siswa } = require("../models");
+
+const index = async (req) => {
+  const user = req.user;
+  const result = await Portofolio.findAll({
+    include: [
+      {
+        model: Siswa,
+        as: "siswa",
+        attributes: { exclude: ["created_at", "updated_at", "kelas_id"] },
+      },
+    ],
+    where: { siswa_id: user.siswa_id },
+    attributes: { exclude: ["created_at", "updated_at", "siswa_id"] },
+  });
+
+  return result;
+};
+
+const show = async (req) => {
+  const { portofolio_id } = req.params;
+  const user = req.user;
+
+  const result = await Portofolio.findOne({
+    where: { id: portofolio_id, siswa_id: user.siswa_id },
+    include: [
+      {
+        model: Siswa,
+        as: "siswa",
+        attributes: { exclude: ["created_at", "updated_at", "kelas_id"] },
+      },
+    ],
+    attributes: { exclude: ["created_at", "updated_at", "siswa_id"] },
+  });
+
+  if (!result) {
+    throw new NotFoundError("Portofolio tidak ada");
+  }
+
+  return result;
+};
+
+module.exports = { show, index };
