8745e8762d9988beae7444f8fbd64b10b009897c
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-08-01 09:00:24 +0700

    modify service absen

--- services/absen.js
+++ services/absen.js
@@ -1,9 +1,18 @@
-const { Kehadiran, Siswa, Absensi_siswa } = require("../models");
-const { NotFoundError } = require("../errors");
+const { Kehadiran, Absensi_siswa } = require("../models");
+const { NotFoundError, BadRequestError } = require("../errors");
+const countRadius = require("../utils/countRadius");
 
 const create = async (req) => {
   const user = req.user;
-  const { kehadiran_id } = req.body;
+  const { kehadiran_id, lat, lon } = req.body;
+
+  const radius = await countRadius(lat, lon, -7.14109, 110.28436);
+
+  console.log(radius);
+
+  if (radius >= 50) {
+    throw new BadRequestError("Anda diluar jangkauan!");
+  }
 
   const checkKehadiran = await Kehadiran.findOne({
     where: { id: kehadiran_id },
